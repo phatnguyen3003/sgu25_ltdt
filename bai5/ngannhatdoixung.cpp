e21e8a96d290ce8a8b7686e31b14bea93d9ecc82
Phát Nguyễn <108886692+phatnguyen3003@users.noreply.github.com>
2025-10-07 09:28:14 +0000

    bai 4 update lan 2

--- bai5/ngannhatdoixung.cpp
+++ bai5/ngannhatdoixung.cpp
@@ -20,8 +20,10 @@ struct vertex{
 vector<int>path_1;
 vector<int>path_2;
 vector<int>sum_path;
-vector<int>pre;
-vector<bool>label;
+vector<int>pre_1;
+vector<int>pre_2;
+vector<bool>label_1;
+vector<bool>label_2;
 vector<vector<vertex>> adjList;
 int dinh,cung,dau,cuoi,giua;
 
@@ -37,7 +39,7 @@ void doc()
     }
 }
 
-void dijkstra(vector<int>&dist,int x)
+void dijkstra(vector<int>&dist,int x,vector<int>&pre,vector<bool>&label)
 {
     dist.resize(dinh+1,INF);
     dist[x]=0;
@@ -75,29 +77,33 @@ void dijkstra(vector<int>&dist,int x)
     }
 }
 
-bool find_path(vector<int>dist_1,vector<int>dist_2)
+bool find_path(vector<int>&dist_1,vector<int>&dist_2)
 {
-    dijkstra(dist_1,dau);
-    dijkstra(dist_2,giua);
+    path_1.clear();
+    path_2.clear();
+    sum_path.clear();
+    dijkstra(dist_1,dau,pre_1,label_1);
+    dijkstra(dist_2,giua,pre_2,label_2);
     if(dist_1[giua]==INF||dist_2[cuoi]==INF)
     {
         return false;
     }
-    for(int u=giua;u!=dau;u=pre[u])
+    for(int u=giua;u!=dau;u=pre_1[u])
     {
         path_1.push_back(u);
     }
     path_1.push_back(dau);
     reverse(path_1.begin(),path_1.end());
 
-    for(int u=cuoi;u!=giua;u=pre[u])
+    for(int u=cuoi;u!=-1 && u!=giua;u=pre_2[u])
     {
         path_2.push_back(u);
     }
-    path_2.push_back(dau);
-    reverse(path_1.begin(),path_1.end());
+    path_2.push_back(giua);
+    reverse(path_2.begin(),path_2.end());
 
-    sum_path.insert(path_1.end(),path_2.begin(),path_2.end());
+    sum_path=path_1;
+    sum_path.insert(sum_path.end(),path_2.begin()+1,path_2.end());
 
     return true;
 }
@@ -110,7 +116,7 @@ int main()
     doc();
     if(find_path(dist_1,dist_2))
     {
-        cout<<sum_path.size()<<" "<<dist_2[cuoi]<<"\n";
+        cout<<sum_path.size()<<" "<<dist_1[giua]+dist_2[cuoi]<<"\n";
         for(int i=0;i<sum_path.size();i++)
         {
             cout<<sum_path[i]<<" ";
