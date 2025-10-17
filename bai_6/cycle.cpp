611360fe5435da7a0bebf9ea0d53ac034b784435
Phát Nguyễn <108886692+phatnguyen3003@users.noreply.github.com>
2025-10-17 15:09:49 +0000

    10:09PM 17/10/2025

--- bai_6/cycle.cpp
+++ bai_6/cycle.cpp
@@ -5,7 +5,28 @@
 #include<cmath>
 
 using namespace std;
+/*
+chuong trinh se duyet qua tat ca hinh tron bang thuat toan dijkstra
+sau do xet xem co duong di tu hinh tron dau tien den hinh tron cuoi cung khong
+neu co thi in ra duong di va so buoc nhau, dong thoi them loai buoc nhay voi moi duong tron di qua no (-1,0,1) voi -1 la khong the di qua hoac la dinh bat dau,
+0 la buoc qua binh thuong, 1 la buoc nhay
 
+ham doc()
+{
+    doc tu input cac hinh tron va luu vao dshtron(danh sach cac hinh tron)
+    sau do khoi tao danh sach ke adjList va ma tran buoc nhay step_jump de luu dinh dang di chuyen giua cac hinh tron trong do moi [i] la so cua hinh tron
+    xet tung hinh tron i va j cho tat ca hinh tron de tinh khoang cach, xet dieu kien de danh dau vao step_jump qua bien khoang_cach
+}
+
+ham duyet() thuc chat la dijkstra
+{
+    trong do khoi tao cac mang visited,parent,dist
+    dat hinh tron dau tien co khoang cach la 0
+    sau do duyet dijkstra nhu binh thuong voi tat ca cac dinh ke cua hinh tron hien tai
+    chi chinh sua dieu kien cap nhat so voi dijkstra binh thuong de uu tien khoang cach sau do moi den xet buoc nhay de co the cap nhat dist va parent}
+
+
+*/
 #define INF 1000000000
 int sohtron,dau,cuoi;
 
@@ -24,8 +45,8 @@ vector<cycle> dshtron;
 vector<vector<int>> adjList;
 vector<vector<int>> step_jump;
 vector<bool> visited;
-vector<pair<int,int>> parent;
-vector<pair<int,int>> dist;
+vector<pair<int,int>> parent; // first la dinh truoc do cua dinh dang xet, second la loai buoc nhay tu dinh truoc den no
+vector<pair<int,int>> dist; // first la so buoc nhay/buoc cua dinh dau den dinh do, second la tong so buoc di chuyen den dinh do
 
 void doc()
 {
@@ -58,12 +79,17 @@ void doc()
                 int r2=dshtron[j-1].r;
 
                 int khoang_cach=sqrt((pow(x2-x1,2) + pow(y2-y1,2))) - (r1 + r2);
-                if(khoang_cach<0) khoang_cach=0;
+                // tinh khoang cach giua 2 hinh tron bang cong thuc khoang cach giua 2 diem trong khong gian, tru di ban binh cac hinh vi dang tim diem gan nhat
+                if(khoang_cach<0) khoang_cach=0;//sau khi tru 2 ban kinh neu khoang cach am(tuc 2 hinh tron giao nhau) thi khoang cach la 0
 
-                if(khoang_cach<=60)
+                else if(khoang_cach<=60)
                 {
                     adjList[i].push_back(j);
                     step_jump[i][j]=(khoang_cach<=50?0:1);
+                }
+                else
+                {
+                    step_jump[i][j]=-1;
                 }
             }
         }
@@ -83,6 +109,7 @@ void duyet()
         int u=-1;
         int min_dist=INF;
         for(int j=1;j<=sohtron;j++)
+        //tim dinh chua tham va co khoang cach nho nhat nhu dijkstra thong thuong
         {
             if(!visited[j] && dist[j].first<min_dist)
             {
